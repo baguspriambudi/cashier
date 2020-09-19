d7ff1002618e03e6a06e22b98c1d819f9147ad37
Wildan Hanani <wildanhanani@gmail.com>
2020-09-19 14:47:08 +0700

    add: unit testing create member and update member

--- test/api_test.js
+++ test/api_test.js
@@ -123,4 +123,79 @@ describe('API Test', () => {
       }
     });
   });
+  describe('POST /api/auth/member/create', () => {
+    it('should return error validation schema', async () => {
+      try {
+        const res = await chai.request(server).post('/api/v1/auth/member/create');
+
+        expect(res.status).to.equal(400);
+        expect(res.body.status).to.equal(400);
+        expect(res.body.message).to.equal('Validation Error');
+        expect(res.body).to.have.property('error');
+      } catch (error) {
+        throw error;
+      }
+    });
+    it('should success register member', async () => {
+      try {
+        const res = await chai.request(server).post('/api/v1/auth/member/create').send({ name: 'suneo' });
+
+        expect(res.status).to.equal(200);
+        expect(res.body.status).to.equal(200);
+        expect(res.body.message).to.equal('Member Successfully inputed');
+        expect(res.body).to.have.property('data');
+        expect(res.body.data).to.have.property('name');
+        expect(res.body.data.name).to.equal('suneo');
+      } catch (error) {
+        throw error;
+      }
+    });
+  });
+  describe('POST /api/v1/auth/member/update', () => {
+    it('should return error validation schema', async () => {
+      try {
+        const res = await chai.request(server).post('/api/v1/auth/member/update');
+
+        expect(res.status).to.equal(400);
+        expect(res.body.status).to.equal(400);
+        expect(res.body.message).to.equal('Validation Error');
+        expect(res.body).to.have.property('error');
+      } catch (error) {
+        throw error;
+      }
+    });
+  });
+  it('should return error member not found', async () => {
+    try {
+      await chai.request(server).post('/api/v1/auth/member/update').send({ memberId: 'M-1611711', expired: 1 });
+      const res = await chai
+        .request(server)
+        .post('/api/v1/auth/member/update')
+        .send({ memberId: 'M-1655211', expired: 1 });
+
+      expect(res.status).to.equal(404);
+      expect(res.body.status).to.equal(404);
+      expect(res.body.message).to.equal('user not found');
+    } catch (error) {
+      throw error;
+    }
+  });
+  it('should success update member', async () => {
+    try {
+      const member = await chai.request(server).post('/api/v1/auth/member/create').send({ name: 'suneo' });
+      const res = await chai.request(server).post('/api/v1/auth/member/update').send({
+        memberId: member.body.data.memberId,
+        expired: 1,
+      });
+      console.log(res.body);
+      expect(res.status).to.equal(200);
+      expect(res.body.status).to.equal(200);
+      expect(res.body.message).to.equal('update successfully');
+      expect(res.body).to.have.property('data');
+      expect(res.body.data.name).to.equal('suneo');
+      expect(res.body.data.diskon).to.equal(5);
+    } catch (error) {
+      throw error;
+    }
+  });
 });
