bfe738473fd986b27291cf00f0bbd1609bf5357e
Wildan Hanani <wildanhanani@gmail.com>
2020-09-22 22:53:57 +0700

    modify: TRX model

--- model/Transactions.js
+++ model/Transactions.js
@@ -1,5 +1,15 @@
 const mongoose = require('mongoose');
 
-const transactionSchema = mongoose.Schema({}, { timestamps: true });
+const datenow = new Date().setHours(0, 0, 0, 0);
+// console.log(datenow);
+
+const date = new Date(datenow);
+// return console.log(date);
+console.log(date);
+const opts = {
+  // Make Mongoose use Unix time (seconds since Jan 1, 1970)
+  timestamps: { currentDate: () => date },
+};
+const transactionSchema = mongoose.Schema({}, opts);
 
 module.exports = mongoose.model('Transaction', transactionSchema);
