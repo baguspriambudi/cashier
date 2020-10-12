08f1a861e5b92e63653d7d0e27b60641378bb997
bagus priambudi <baguspriambudi77@gmail.com>
2020-10-13 00:05:13 +0700

    modify: mid/auth

--- route/user_route.js
+++ route/user_route.js
@@ -2,10 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
-const auth = require('../middlleware/auth');
 const Schema = require('../middlleware/Schema');
 const userController = require('../controller/user');
 
-router.post('/create', auth.isAdmin,Schema.midRegister, userController.createuser);
+router.post('/create', Schema.midRegister, userController.createuser);
 
 module.exports = router;
